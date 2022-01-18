49b5d6e5f416ad00eced94dba8589bb2be950dca
lax33 <lax33@tut.by>
2022-01-18 21:59:34 +0300

    Add project files.

---
+++ Linq.NET5/Student.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Linq.NET5
+{
+    internal class Student
+    {
+        public string First { get; internal set; }
+        public string Last { get; internal set; }
+        public int ID { get; internal set; }
+        public List<int> Scores { get; internal set; }
+        public string Street { get; internal set; }
+        public string City { get; internal set; }
+    }
+}
---
+++ Linq.NET5/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace Linq.NET5
+{
+    internal class Teacher
+    {
+        public string First { get; internal set; }
+        public string Last { get; internal set; }
+        public int ID { get; internal set; }
+        public string City { get; internal set; }
+    }
+}
