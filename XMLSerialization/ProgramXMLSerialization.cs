793bfd4a81616047af03b7f9f91bcf42fc13bd97
lax33 <lax33@tut.by>
2022-01-25 21:11:27 +0300

    1

---
+++ XMLSerialization/ProgramXMLSerialization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+    internal class ProgramXMLSerialization
+    {
+        static void Main(string[] args)
+        {
+            File.Delete("Fl.xml");
+            Seriaz person = new Seriaz("Tom", 12, "IIT");
+
+            XmlSerializer ser=new XmlSerializer(typeof(Seriaz));
+
+            using (FileStream fl=new FileStream("Fl.xml", FileMode.OpenOrCreate))
+            {
+                
+                ser.Serialize(fl, person);
+            }
+        }
+    }
+}
---
+++ XMLSerialization/Seriaz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLSerialization
+{
+    [Serializable]
+    public class Seriaz:BaseSer
+    {
+        public string userName;
+        public int userAge;
+
+        public Seriaz(){}
+        public Seriaz(string userName, int userAge, string company) :base(company)
+        {
+            this.userAge=userAge;
+            this.userName=userName;
+        }
+    }
+}
