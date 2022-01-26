d5f0fe630c906e179eb053a617cbb4c6f9017084
lax33 <lax33@tut.by>
2022-01-26 17:34:29 +0300

    1

--- XMLSerialization/ProgramXMLSerialization.cs
+++ XMLSerialization/ProgramXMLSerialization.cs
@@ -9,14 +9,23 @@ namespace XMLSerialization
         static void Main(string[] args)
         {
             File.Delete("Fl.xml");
-            Seriaz person = new Seriaz("Tom", 12, "IIT");
+            //Seriaz person = new Seriaz("Tom", 12, "IIT");
+            //Car car=new Car();
 
-            XmlSerializer ser=new XmlSerializer(typeof(Seriaz));
+            //XmlSerializer ser=new XmlSerializer(typeof(Seriaz));
+            //XmlSerializer sercar=new XmlSerializer(typeof(Car));
 
-            using (FileStream fl=new FileStream("Fl.xml", FileMode.OpenOrCreate))
+           
+
+            using (FileStream fl=new FileStream("Fl.xml", FileMode.Append))
             {
-                
+                Seriaz person = new Seriaz("Tom", 12, "IIT");
+                Car car = new Car();
+
+                XmlSerializer ser = new XmlSerializer(typeof(Seriaz));
+                XmlSerializer sercar = new XmlSerializer(typeof(Car));
                 ser.Serialize(fl, person);
+                sercar.Serialize(fl, car);
             }
         }
     }
